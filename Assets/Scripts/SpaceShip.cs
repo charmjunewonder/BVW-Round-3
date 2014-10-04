edc6b8d30b06c32f06c2f4b7f4a9ea0cb0ce4aee
Eric <charmjunewonder@gmail.com>
2014-10-03 21:26:36 -0400

    add collision and explosion

--- Assets/Scripts/SpaceShip.cs
+++ Assets/Scripts/SpaceShip.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class SpaceShip : MonoBehaviour {
 
+	public GameObject explosion;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +12,12 @@ public class SpaceShip : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.collider.tag == "Meteor") {
+			//explosion.SetActive(true);
+			Debug.Log("Collide! Loss!");
+		}
 	}
 }
