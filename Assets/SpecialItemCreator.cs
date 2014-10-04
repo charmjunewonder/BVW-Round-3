8b45832e3475747735157a4eaadd05c37e56b09e
Eric <charmjunewonder@gmail.com>
2014-10-04 12:43:00 -0400

    specialItem

---
+++ Assets/SpecialItemCreator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpecialItemCreator : MonoBehaviour {
+	public GameObject[] specialItems; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator createSpecialItem(){
+		while(true){
+			foreach(GameObject si in specialItems){
+
+			}
+		}
+	}
+}
