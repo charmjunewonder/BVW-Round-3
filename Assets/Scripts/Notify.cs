03c934203fbe8005466b8a42805e917f81775ad1
Eric <charmjunewonder@gmail.com>
2014-10-05 18:58:36 -0400

    start scene finished

--- Assets/Scripts/Notify.cs
+++ Assets/Scripts/Notify.cs
@@ -3,32 +3,33 @@ using System.Collections;
 
 public class Notify : MonoBehaviour
 {
-		public Texture2D[] textures;
+	public Texture2D[] textures;
+	bool isStart = false;
+	// Use this for initialization
+	void Start ()
+	{
 
-		// Use this for initialization
-		void Start ()
-		{
-	
-		}
-	
-		// Update is called once per frame
-		void Update ()
-		{
-				if (StartGame.begin == true) {
-						renderer.enabled = true;
-						StartCoroutine (NotifyPlayer ());
-				}
-	
-		}
-		IEnumerator NotifyPlayer ()
-		{
-				for (int i=0; i<4; i++) {
-						renderer.material.mainTexture = textures [i];
-						yield return new WaitForSeconds (1f);
+	}
 
-				}
-				if (renderer.material.mainTexture == textures [3])
-						Application.LoadLevel ("GamePlay");
-				
+	// Update is called once per frame
+	void Update ()
+	{
+		if (StartGame.begin && !isStart) {
+			renderer.enabled = true;
+			StartCoroutine (NotifyPlayer ());
+			isStart = true;
 		}
+
+	}
+	IEnumerator NotifyPlayer ()
+	{
+			for (int i=0; i<4; i++) {
+					renderer.material.mainTexture = textures [i];
+					yield return new WaitForSeconds (1f);
+
+			}
+			if (renderer.material.mainTexture == textures [3])
+					Application.LoadLevel ("GamePlay");
+			
+	}
 }
--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -4,89 +4,89 @@ using System.Collections;
 public class StartGame : MonoBehaviour
 {
 	
-		public JamoDrum jod;
-	
-		public GameObject[] spinners = new GameObject[4];
-		public GameObject[] drums = new GameObject[4];
-	
-		public Material[] starMaterials = new Material[4];
-		public GameObject star;
-		public float[] degPerTick = new float[4];
-		public float[] spinnerAngle = new float[4];
-		public Texture[] normalDrumTexture;
-		public Texture[] hittedDrumTexture;
-		public GameObject spaceShip;
-		private float[] initAngle = new float[4];
-	
-		private bool once;
-		private Vector3[] direction;
+	public JamoDrum jod;
 
-		public static bool begin;
-		// Use this for initialization
-		void Start ()
-		{
-				direction = new Vector3[4];
-				direction [0] = new Vector3 (-1, 0, 1);
-				direction [1] = new Vector3 (1, 0, 1);
-				direction [2] = new Vector3 (1, 0, -1);
-				direction [3] = new Vector3 (-1, 0, -1);
-		
-				for (int i=0; i<4; i++) {
-						initAngle [i] = spinners [i].transform.rotation.eulerAngles.y;
-				}
+	public GameObject[] spinners = new GameObject[4];
+	public GameObject[] drums = new GameObject[4];
+	public GameObject[] colorNotify;
+	public GameObject fourColorNotify;
+	public GameObject notify;
+	public Material[] starMaterials = new Material[4];
+	public GameObject star;
+	public float[] degPerTick = new float[4];
+	public float[] spinnerAngle = new float[4];
+	public Texture[] normalDrumTexture;
+	public Texture[] hittedDrumTexture;
+	public GameObject spaceShip;
+	private float[] initAngle = new float[4];
+
+	private bool once;
+
+	public static bool begin;
+	// Use this for initialization
+	void Start ()
+	{
+		for (int i=0; i<4; i++) {
+			initAngle [i] = spinners [i].transform.rotation.eulerAngles.y;
 		}
-	
-		// Update is called once per frame
-		void Update ()
-		{
-			if (!once) {
-				once = true;
-				jod.AddHitEvent (HitHandler);
-				jod.AddSpinEvent (SpinHandler);
-			}
-			for (int i=0; i<4; i++) {
-				//spin
-				if (Mathf.Abs (jod.spinDelta [i]) > 0) {
-					//				Debug.Log("EXAMPLE SPIN "+i);
-					spinnerAngle [i] += jod.spinDelta [i] * degPerTick [i];
-					//spinnerAngle[i] += jod.spinDelta[i];
-					//				Debug.Log("Spin "+i+" : "+jod.spinDelta[i]);
-					spinnerAngle [i] = Mathf.Repeat (spinnerAngle [i], 360);
-					Vector3 rot = spinners [i].transform.rotation.eulerAngles;
-					rot.y = initAngle [i] + spinnerAngle [i];
-					spinners [i].transform.rotation = Quaternion.Euler (rot);
-	
-				}
-				//hit
-				if (jod.hits [i] > 0) {
-					Debug.Log (jod.hits [i]);
-					drums [i].renderer.material.mainTexture = hittedDrumTexture [i];
-				} else {
-					drums [i].renderer.material.mainTexture = normalDrumTexture [i];
-	
-				}
-			}
+	}
 
-			if ((jod.hits [0] > 0) && (jod.hits [1] > 0) && (jod.hits [2] > 0) && (jod.hits [3] > 0)) {
-				begin = true;
-			}
-
-
-
-			if (Input.GetKeyUp (KeyCode.Escape)) {
-					Application.Quit ();
-			}
-				
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!once) {
+			once = true;
+			jod.AddHitEvent (HitHandler);
+			jod.AddSpinEvent (SpinHandler);
 		}
-	
-		public void SpinHandler (int controllerID, int delta)
-		{
-				//		Debug.Log("SPIN EVENT "+(controllerID-1));
+		for (int i=0; i<4; i++) {
+			//spin
+			if (Mathf.Abs (jod.spinDelta [i]) > 0) {
+				//				Debug.Log("EXAMPLE SPIN "+i);
+				spinnerAngle [i] += jod.spinDelta [i] * degPerTick [i];
+				//spinnerAngle[i] += jod.spinDelta[i];
+				//				Debug.Log("Spin "+i+" : "+jod.spinDelta[i]);
+				spinnerAngle [i] = Mathf.Repeat (spinnerAngle [i], 360);
+				Vector3 rot = spinners [i].transform.rotation.eulerAngles;
+				rot.y = initAngle [i] + spinnerAngle [i];
+				spinners [i].transform.rotation = Quaternion.Euler (rot);
+
+			}
+			//hit
+			if (jod.hits [i] > 0) {
+				drums [i].renderer.material.mainTexture = hittedDrumTexture [i];
+				colorNotify[i].SetActive(true);
+			} else {
+				drums [i].renderer.material.mainTexture = normalDrumTexture [i];
+
+			}
 		}
-	
-		public void HitHandler (int controllerID)
-		{
-				//		Debug.Log("HIT EVENT "+(controllerID-1));
+
+
+		if ((jod.hits [0] > 0) && (jod.hits [1] > 0) && (jod.hits [2] > 0) && (jod.hits [3] > 0)) {
+			begin = true;
+			fourColorNotify.SetActive(false);
+
+			notify.SetActive(true);
 		}
+
+		if (Input.GetKeyUp (KeyCode.Escape)) {
+				Application.Quit ();
+		}
+		for(int i = 0; i < 4; i++)
+		{
+			jod.spinDelta[i] = 0;
+		}
+	}
+
+	public void SpinHandler (int controllerID, int delta)
+	{
+			//		Debug.Log("SPIN EVENT "+(controllerID-1));
+	}
+
+	public void HitHandler (int controllerID)
+	{
+			//		Debug.Log("HIT EVENT "+(controllerID-1));
+	}
 	
 }
