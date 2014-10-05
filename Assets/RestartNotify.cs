f3ed584f633a526e0564c30d59410826178c3092
Eric <charmjunewonder@gmail.com>
2014-10-05 13:22:24 -0400

    End scene

---
+++ Assets/RestartNotify.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartNotify : MonoBehaviour {
+
+	public Texture[] numbersTexture = new Texture[10];
+	public GameObject[] currentScore;
+	public GameObject[] highestScore;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+}
